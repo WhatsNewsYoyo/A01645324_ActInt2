3f6529e5cf6b2ae141bec908ebeb466f57fe6970
José Yael Varela García <132051688+WhatsNewsYoyo@users.noreply.github.com>
2025-11-09 19:57:46 -0600

    New versions, check probe3 for without any CGAL intervention while check probe2 for a non functional try with cgal

--- build/CMakeFiles/CGAL_Qt5_moc_and_resources.dir/qrc_CGAL.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/CGAL_Qt5_moc_and_resources.dir/qrc_CGAL.cpp.o: \
- /home/victor/Escuela/Quinto\ semestre/A01645324_ActInt2/build/qrc_CGAL.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/CGAL_Qt5_moc_and_resources.dir/qrc_Input.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/CGAL_Qt5_moc_and_resources.dir/qrc_Input.cpp.o: \
- /home/victor/Escuela/Quinto\ semestre/A01645324_ActInt2/build/qrc_Input.cpp \
- /usr/include/stdc-predef.h
